8ba8701ffdb8082910b2587c43d6eff4e6d16d81
ulvikrml <ulvikrml@gmail.com>
2023-05-04 18:35:06 +0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        mainBlue : '#00C7FF'
+      }
+    },
+  },
+  plugins: [],
+}
+
